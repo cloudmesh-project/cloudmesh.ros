de784156cc2ef800153b070948b2f58b2271d9ca
eunosm3 <lawsonma@iu.edu>
2017-04-30 14:45:09 -0400

    usr:chg: update repo dir structure

--- rosProjFinal/mybot_ws/build/mybot_description/gtest/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /usr/src/gtest
-# Build directory: /media/storage/github/cloudmesh/cloudmesh.ros/rosProjFinal/mybot_ws/build/mybot_description/gtest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
